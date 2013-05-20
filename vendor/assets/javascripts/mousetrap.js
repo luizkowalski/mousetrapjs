ce6244b3a33f82d337a0e76e3eea70a16a1226b9
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2013-05-20 11:26:18 -0300

    Latest js

--- vendor/assets/javascripts/mousetrap.js
+++ vendor/assets/javascripts/mousetrap.js
@@ -17,7 +17,7 @@
  * Mousetrap is a simple keyboard shortcut library for Javascript with
  * no external dependencies
  *
- * @version 1.3.2
+ * @version 1.4.0
  * @url craig.is/killing/mice
  */
 (function() {
@@ -125,7 +125,8 @@
             'option': 'alt',
             'command': 'meta',
             'return': 'enter',
-            'escape': 'esc'
+            'escape': 'esc',
+            'mod': /Mac|iPod|iPhone|iPad/.test(navigator.platform) ? 'meta' : 'ctrl'
         },
 
         /**
@@ -275,14 +276,14 @@
      * @param {Object} doNotReset
      * @returns void
      */
-    function _resetSequences(doNotReset, maxLevel) {
+    function _resetSequences(doNotReset) {
         doNotReset = doNotReset || {};
 
         var activeSequences = false,
             key;
 
         for (key in _sequenceLevels) {
-            if (doNotReset[key] && _sequenceLevels[key] > maxLevel) {
+            if (doNotReset[key]) {
                 activeSequences = true;
                 continue;
             }
@@ -434,6 +435,13 @@
             maxLevel = 0,
             processedSequenceCallback = false;
 
+        // Calculate the maxLevel for sequences so we can only execute the longest callback sequence
+        for (i = 0; i < callbacks.length; ++i) {
+            if (callbacks[i].seq) {
+                maxLevel = Math.max(maxLevel, callbacks[i].level);
+            }
+        }
+
         // loop through matching callbacks for this key event
         for (i = 0; i < callbacks.length; ++i) {
 
@@ -443,11 +451,20 @@
             // callback for matching g cause otherwise you can only ever
             // match the first one
             if (callbacks[i].seq) {
-                processedSequenceCallback = true;
 
-                // as we loop through keep track of the max
-                // any sequence at a lower level will be discarded
-                maxLevel = Math.max(maxLevel, callbacks[i].level);
+                // only fire callbacks for the maxLevel to prevent
+                // subsequences from also firing
+                //
+                // for example 'a option b' should not cause 'option b' to fire
+                // even though 'option b' is part of the other sequence
+                //
+                // any sequences that do not match here will be discarded
+                // below by the _resetSequences call
+                if (callbacks[i].level != maxLevel) {
+                    continue;
+                }
+
+                processedSequenceCallback = true;
 
                 // keep a list of which sequences were matches for later
                 doNotReset[callbacks[i].seq] = 1;
@@ -457,16 +474,24 @@
 
             // if there were no sequence matches but we are still here
             // that means this is a regular match so we should fire that
-            if (!processedSequenceCallback && !_sequenceType) {
+            if (!processedSequenceCallback) {
                 _fireCallback(callbacks[i].callback, e, callbacks[i].combo);
             }
         }
 
-        // if you are inside of a sequence and the key you are pressing
-        // is not a modifier key then we should reset all sequences
-        // that were not matched by this key event
+        // if the key you pressed matches the type of sequence without
+        // being a modifier (ie "keyup" or "keypress") then we should
+        // reset all sequences that were not matched by this event
+        //
+        // this is so, for example, if you have the sequence "h a t" and you
+        // type "h e a r t" it does not match.  in this case the "e" will
+        // cause the sequence to reset
+        //
+        // modifier keys are ignored because you can have a sequence
+        // that contains modifiers such as "enter ctrl+space" and in most
+        // cases the modifier key will be pressed before the next key
         if (e.type == _sequenceType && !_isModifier(character)) {
-            _resetSequences(doNotReset, maxLevel);
+            _resetSequences(doNotReset);
         }
     }
 
